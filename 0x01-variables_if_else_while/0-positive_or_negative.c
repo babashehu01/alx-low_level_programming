183e8ddc2276500370a021acdec4c956e38cd381
babashehu01 <shettimababashehu57@gmail.com>
2022-03-11 19:59:44 +0000

    Program to check positive or negative integers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Entry point
+  * Return: Always 0 (success)
+  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* Checking if n is positive or negaive */
-	if (n < 0)
+	/* your code goes there */
+	if (n > 0)
 	{
-		printf("%d is negative\n", n);
+		printf ("%d is positive\n", n);
 	}
-	else if (n > 0)
+	else if (n == 0)
 	{
-		printf("%d is positive\n", n);
+		printf ("%d is zero\n", n;);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is negative\n", n)
 	}
 	return (0);
 }
