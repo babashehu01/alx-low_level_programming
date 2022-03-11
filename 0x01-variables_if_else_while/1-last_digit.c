24407010ec52f03b8c69d441f1cdc5a0a54db0b1
babashehu01 <shettimababashehu57@gmail.com>
2022-03-11 22:18:31 +0000

    Added the program that finds the last digit of an integer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,9 +28,9 @@ int main(void)
 
 	if (ld > 5)
 	{
-		printf("Last digit of %d is %d and is grater than 5\n", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if (ld == 0) 
+	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
