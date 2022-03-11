317b9c9675dff62c377a3df81e424c3b1ee996a4
babashehu01 <shettimababashehu57@gmail.com>
2022-03-11 23:06:13 +0000

    Added program that prints a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  *
+  * Author: Babashehu Shettima Musti
+  * Date: 11-March-2021
+  * Program: Prints letter a-z
+  * ch = Character
+  */
+
+int main(void)
+{
+	int i;
+	int ch;
+
+	for (i = 97; i <= 122; i++)
+	{
+		puts(ch);
+	}
+	return (0);
+}
