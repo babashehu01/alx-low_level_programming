61d845f6e31666bdc09567e05a62c99b4f7fcc9e
babashehu01 <shettimababashehu57@gmail.com>
2022-03-12 00:58:39 +0000

    Added program that prints base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  *
+  * Author: Babashehu Shettima Musti
+  * Date: 2-March-2022
+  * Program: Prints base 10 numbers
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
