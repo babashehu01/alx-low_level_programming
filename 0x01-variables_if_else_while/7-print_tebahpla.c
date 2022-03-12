079c5714c70454485b57b7ff162fc871d207610a
babashehu01 <shettimababashehu57@gmail.com>
2022-03-12 01:08:47 +0000

    Added program that reverses an int

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  *
+  * Author: Babashehu Shettima Musti
+  * Date: 2-March-2022
+  * Program: Prints integers in reverse
+  */
+int main(void)
+{
+	int i;
+
+	for (int i = 122; i > 97; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
