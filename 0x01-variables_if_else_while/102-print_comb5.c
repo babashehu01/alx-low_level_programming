222c27c1d8d925a0bcba8d7c21dc6efb62a67b99
babashehu01 <shettimababashehu57@gmail.com>
2022-03-15 01:17:00 +0000

    Added 102-print_comb4.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i, j, k, l, op1, op2;
+
+	for (l = 48; l <= 57; l++)
+	{
+		for(k = 48; k <= 57; k++)
+		{
+			for(j = 48; j <= 57)
+			{
+				for(i = 48; i <= 57; i++)
+				{
+					op1 = (l * 10) + k;
+					op2 = (j * 10) + i;
+					if (op1 < op2)
+					{
+						putchar(l);
+						putchar(k);
+						putchar(' ');
+						putchar(j);
+						putchar(i);
+						if (l == 57 && k == 56 && j == 57 && i == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
