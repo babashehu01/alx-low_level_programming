8c5f9ab027a7922f3acb228122fa6ea6dff987ff
babashehu01 <shettimababashehu57@gmail.com>
2022-03-15 23:54:17 +0000

    Added is_lower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _islower - checks if a char is lower or not
+  *
+  * Return: 1 for lower and 0 for not lower
+  * @c: Character in ASCII
+  */
+int _islower(int c)
+{
+	if (c >= 122 && c <= 97)
+		return (1);
+	else
+		return (0);
+}
