86c9eb80fcd21ad14013ede7d81e4c1b0eda003c
babashehu01 <shettimababashehu57@gmail.com>
2022-03-15 23:31:32 +0000

    Added function that prints all lower alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,8 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
+  * print_alphabet: prints the alphabet, in lowercase
   */
-
-/* prototyping the print_alphabet */
-void print_alphabet(void);
-
-/* main */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/* Function definition(Called function) */
 void print_alphabet(void)
 {	int i;
 	for (i = 97; i <= 122; i++)
