1fd365567201fee8737908d42435719581caa9ab
babashehu01 <shettimababashehu57@gmail.com>
2022-03-16 00:56:05 +0000

    int print_last_digit(int);

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_last_digit - Checks for the last digit of an int
+  *
+  * Return: Returns the last digit
+  * @n: Is the absolute arguement
+  */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
