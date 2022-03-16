4b725d16cc400e8352b56279be2efe0cf3231678
babashehu01 <shettimababashehu57@gmail.com>
2022-03-16 00:48:56 +0000

    Added 6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _abs - Calculated absolute value
+  *
+  * Return: Absolute value
+  * @n: takes an int values
+  */
+int _abs(int n)
+{
+	unsigned int n;
+
+	return (n);
+}
