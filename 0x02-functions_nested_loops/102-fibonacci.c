a1cd539ea4d1b69b423a90e4f9218fb47d7cd9c3
babashehu01 <shettimababashehu57@gmail.com>
2022-03-16 22:15:58 +0000

    Added first 50 fibbonaci function

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i, n;
+
+	/*initialize first and second terms*/
+	int t1 = 1;
+	int t2 = 2;
+
+	/* initialize the next term (3rd term)*/
+	int nextTerm = t1 + t2;
+
+	/* print the first two terms t1 and t2*/
+	printf("%d, %d ", t1, t2);
+
+	/* print 3rd to 50th terms*/
+	for (i = 3; i <= 50; ++i)
+	{
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	return (0);
+}
