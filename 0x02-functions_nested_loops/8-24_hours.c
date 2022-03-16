224eb6d35f6d29cd4ac224437dc52c916f4a6dbd
babashehu01 <shettimababashehu57@gmail.com>
2022-03-16 02:12:30 +0000

    Added 9-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+  * jack_bauer - Prints the minutes of a day
+  */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (j = 48; i <= 50; i++)
+	{
+		for (j = 48; j <= 51; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
