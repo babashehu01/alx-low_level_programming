d754df6ba7d6a868530f5d29b0c49c5aa0200095
babashehu01 <shettimababashehu57@gmail.com>
2022-03-17 21:09:47 +0000

    Added Fizz Buzz Program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - checks fizz buzz and either
+  * Return: 0 on success and 1 for a fault
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+	return (0);
+}
