ce641890cb51efd1e6bb0b470c70885e80a00908
babashehu01 <shettimababashehu57@gmail.com>
2022-03-17 19:55:12 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_line - Prints _
+  * Return: Does not return any value
+  * @n: Number of _ to be printed
+  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+			break;
+		else
+			_putchar('_');
+		_putchar('\n');
+	}
+}
