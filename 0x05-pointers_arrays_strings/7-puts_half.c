a02c067d326d9192c8152ae701529eaa5802a68c
babashehu01 <shettimababashehu57@gmail.com>
2022-03-21 23:37:46 +0000

    Added 7-puts2.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * puts_half - prints half of a string
+  * @str: is the string to be printed
+  * return: no return value
+  */
+void puts_half(char *str)
+{
+	int n, i = 0, length = 0;
+
+	/* finding the length */
+	while (str[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+
+	/* checking conditions for printing */
+	for (i = 0; i < length)
+		if (length % 2 != 0)
+		{
+			n = (length - 1) / 2;
+			_putchar(n);
+		}
+	_putchar('\n');
+}
