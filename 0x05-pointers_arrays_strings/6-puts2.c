7fc088252271ed32406bb68c7090cda0ff4d50fc
babashehu01 <shettimababashehu57@gmail.com>
2022-03-21 22:54:10 +0000

    added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * puts2 - prints every other character of a string
+  * @str: holds the string to be printed
+  * return: no return value
+  */
+void puts2(char *str)
+{
+	int i;
+
+	for (i  = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+}
