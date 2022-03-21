52469b645a208e3014395c5ffd2572cfcc3828b5
babashehu01 <shettimababashehu57@gmail.com>
2022-03-21 21:09:31 +0000

    Added 0_reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - resets a value to 98
+  * Return: no return value
+  * @*n: holds the value to be resetted
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
