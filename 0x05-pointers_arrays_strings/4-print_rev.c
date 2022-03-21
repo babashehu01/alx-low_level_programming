01275775698dc8cf78db7bbea5025ff5f9bbbfc3
babashehu01 <shettimababashehu57@gmail.com>
2022-03-21 22:16:28 +0000

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - reverses a string
+  * @s: is the string to be reversed
+  * return: no return value
+  */
+void print_rev(char *s)
+{
+	int i, length;
+
+	i = 0, lenght = 0;
+	/* getting the length of the string */
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	/* printing the string from descending order */
+	for (i = length; i >= 0; i--)
+		_putchar(s[i]);
+	/* New line at the end */
+	_putchar('\n');
+}
