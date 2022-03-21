5fa3e1f5c20c91d04ccc50887c54ab38a5bab831
babashehu01 <shettimababashehu57@gmail.com>
2022-03-21 22:41:30 +0000

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * re_string - reverses a string
+  * @s: is the reversing string
+  * return: no return value
+  */
+void re_string(char *s)
+{
+	int i = 0, length 0, swap;
+
+	/* getting the length of the string */
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	/* swaping the strings */
+	for (i = 0; i <= length / 2; i++)
+	{
+		swap = s[i];
+		s[i] = s[length - i];
+	}
+	/* printing the string */
+	for (i = 0; i < length; i++)
+	{
+		_putchar(s[i]);
+	}
+}
