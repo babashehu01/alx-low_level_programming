fb117ca30a0236175c1f3693e0400d9889364223
babashehu01 <shettimababashehu57@gmail.com>
2022-03-21 21:45:32 +0000

    Added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string to stdout
+  * @str: is the inputted string
+  * Return: no return value
+  */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	length = 0;
+	/* finding the length of the string */
+	while (str[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	/* accessing the string */
+	for (i = 0; i < length; i++)
+	{
+		/* printing the string */
+		write(1, str[i], 1);
+	}
+
+
+}
