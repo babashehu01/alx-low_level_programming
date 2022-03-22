6795730e7086a1a71ab907dcd81cf91dc73b2208
babashehu01 <shettimababashehu57@gmail.com>
2022-03-22 01:02:25 +0000

    Added 7-puts_half.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,13 +18,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
-		{
 			_putchar(a[i]);
 			_putchar(',');
-		}
-		else
-			_putchar(a[i]);
 	}
 	_putchar('\n');
 }
