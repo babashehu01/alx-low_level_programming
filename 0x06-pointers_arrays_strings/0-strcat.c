02e2af48644a0ccd057eb0e5b485a3495ea88bef
babashehu01 <shettimababashehu57@gmail.com>
2022-03-23 13:24:36 +0000

    Added 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - joins two strings together
+ * @dest: where it is going to be joint to
+ * @src: the string to be joint
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+        int i = 0, j, len1 = 0;
+        /* getting the destination length */
+        while(dest[i] != '\0')
+        {
+                len1++;
+                i++;
+        }
+        for (i = len1 + 1, j = 0; src[i] != '\0'; i++, j++)
+        {
+                dest[i] = src[j];
+        }
+        return (dest);
+        _putchar('\n');
+
+}
+
