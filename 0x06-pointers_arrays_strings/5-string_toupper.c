772db00bb87f5d7b3be370787a80dfd46773d924
babashehu01 <shettimababashehu57@gmail.com>
2022-03-24 16:46:58 +0000

    Added 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,26 +7,19 @@
  */
 char *string_toupper(char *c)
 {
-        int i = 0, str_len = 0;
+	int i = 0, str_len = 0;
 
-        /* getting the string length */
-        while (c[i] != '\0')
-        {
-                str_len++;
-                i++;
-        }
-
-        /* modifying the string */
-        for (i = 0; i < str_len; i++)
-                /* check if its lower */
-                if (c[i] >= 97 && c[i] <= 122)
-                {
-                        i-=32;
-                        _putchar(c[i]);
-                }
-                /* all other characters are valid */
-                else
-                        _putchar(c[i]);
-        return (c);
+	/* getting the string length */
+	while (c[i] != '\0')
+	{
+		str_len++;
+		i++;
+	}
+	/* modifying the string */
+	for (i = 0; i < str_len; i++)
+		/* check if its lower */
+		if (c[i] >= 97 && c[i] <= 122)
+			c[i] -= 32;
+	return (c);
 }
 
