be9dbfe2f52a8ff85a431443a74096c3523601d3
babashehu01 <shettimababashehu57@gmail.com>
2022-03-24 16:39:53 +0000

    Added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to upper
+ * @c: character to be capitalized
+ * Return: modified string
+ */
+char *string_toupper(char *c)
+{
+        int i = 0, str_len = 0;
+
+        /* getting the string length */
+        while (c[i] != '\0')
+        {
+                str_len++;
+                i++;
+        }
+
+        /* modifying the string */
+        for (i = 0; i < str_len; i++)
+                /* check if its lower */
+                if (c[i] >= 97 && c[i] <= 122)
+                {
+                        i-=32;
+                        _putchar(c[i]);
+                }
+                /* all other characters are valid */
+                else
+                        _putchar(c[i]);
+        return (c);
+}
+
