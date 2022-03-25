29a96cfe1fb985a08b2eef46ab799165158df35f
babashehu01 <shettimababashehu57@gmail.com>
2022-03-25 00:49:00 +0000

    Added 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: to destination
+ * @src: from src
+ * @n: number of characters to be copied
+ * Return: a pointer to the copy of the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
