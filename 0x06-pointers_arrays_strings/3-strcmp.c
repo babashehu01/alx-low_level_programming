7b45c5b6c3247b833401193a3c49c12332f53ba9
babashehu01 <shettimababashehu57@gmail.com>
2022-03-25 01:15:18 +0000

    Added 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,18 +4,18 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if equal
+ * Return: 0 if equal or j if not
  */
 _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j;
 
-	i = 0;
+	i = 0, j = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1 == s2)
 	{
 		/* Adding values of si and s2 */
-		s1++;
-		s2++;
+		j = s1[i] - s2[i];
+		i++;
 	}
-	return (s1 - s2);
+	return (j);
 }
