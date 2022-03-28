65194ebdebf5a1247c138e655a412165f80707c6
babashehu01 <shettimababashehu57@gmail.com>
2022-03-28 10:49:41 +0000

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - checks for a special character in a string
+ * @s: is the string to be checked
+ * @c: store the first occurence of a character
+ * 
+ * Return: @c
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (c);
+			break;
+		}
+		i++;
+	}
+}
