ae310e15c0abd5730330ad199ee28dc0d0a0e71e
babashehu01 <shettimababashehu57@gmail.com>
2022-03-31 16:16:28 +0000

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+  * main - prints all possible
+  *combination of a single digit number
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i);
+			_putchar(j);
+			_putchar(',');
+			_putchar(' ');
+		}
+
+	}
+}
