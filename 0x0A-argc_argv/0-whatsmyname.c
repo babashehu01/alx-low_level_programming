baad4413737770fc43fd42a9a01102e9bd75d410
babashehu01 <shettimababashehu57@gmail.com>
2022-04-01 09:50:37 +0000

    Added program that prints it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the program name
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argc[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
