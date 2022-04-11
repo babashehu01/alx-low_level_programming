9e2eacd564bfb8d354482da586a02a62ed7e99ef
babashehu01 <shettimababashehu57@gmail.com>
2022-04-11 23:19:38 +0000

    Added object like macro file

--- 0x0D-preprocessor/0-object-like_macro.h
+++ 0x0D-preprocessor/0-object-like_macro.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * file: 0-object_like_macro.h
+ * File: 0-object_like_macro.h
  * Desc: defines a macro named size as an abreviation for 1024
  */
 #define SIZE 1024
