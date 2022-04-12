19773f28fba8802e66c338bec57025cb42b248d3
babashehu01 <shettimababashehu57@gmail.com>
2022-04-12 00:35:57 +0000

    Added a function that initializes astructure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: dog to be initialized
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
