3266888f66cadaf4592b639ea63306a362247aa2
babashehu01 <shettimababashehu57@gmail.com>
2022-04-12 00:38:24 +0000

    Added a function that initializes a structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: dog to be initialized
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
+ *
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
