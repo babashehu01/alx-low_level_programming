5a1f3198e9a5862cd51803920a15e5c340abee17
babashehu01 <shettimababashehu57@gmail.com>
2022-04-13 16:54:56 +0000

    Added function that prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,9 @@
  * print_name - prints name
  * @name: name to be printed
  * @f: pointer to printing function
+ * Return: no return value
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
 		f(name);
 }
