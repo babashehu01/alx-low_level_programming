144f7233ec28f3921ebe4197aa0f31ee07774137
babashehu01 <shettimababashehu57@gmail.com>
2022-04-15 22:32:26 +0000

    Fixed the function that prints a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,14 +18,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
+		str = va_arg(list, char *);
 		if (!str)
-			printf("nil");
-		else
+			str = "(nil)";
+		if (!separator)
 			printf("%s", str);
-		if (i < n - 1)
-			if (separator)
-				printf("%s", separator);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
 	}
 	va_end(args);
 	printf("\n");
