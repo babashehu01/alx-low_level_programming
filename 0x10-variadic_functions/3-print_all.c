2a335927a2b8f7a8fe19866efeaaaa37c429171e
babashehu01 <shettimababashehu57@gmail.com>
2022-04-15 22:53:14 +0000

    Fixed function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,51 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
+
 /**
- * print_all - prints everything
- * @format: format specifier
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
- * Return: void
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-	int chars = strlen(format);
-	char separator[] = ", ";
-	int i;
-	va_list args;
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-	va_start(args, format);
-	if (format)
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		while (format[i])
+		j = 0;
+		while (t_arg[j])
 		{
-			switch (format[i])
+			if (format[i] == t_arg[j] && c)
 			{
-				case 'c':
-					printf("%s%c", sep, va_arg(args, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(args, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(args, double));
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			sep = ", ";
-			i++;
+				printf(", ");
+				break;
+			} j++;
 		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
 	}
-	va_end(args);
-	printf("\n");
+	printf("\n"), va_end(valist);
 }
