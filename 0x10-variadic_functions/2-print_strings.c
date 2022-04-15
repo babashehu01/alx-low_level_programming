93ad42a1615580bdc396222426460b28283d49b7
babashehu01 <shettimababashehu57@gmail.com>
2022-04-15 20:19:05 +0000

    Fixed the function that prints a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,8 +23,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nil");
 		else
 			printf("%s", str);
-		if (separator && i < n - 1)
-			printf("%s", separator);
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
 	va_end(args);
 	printf("\n");
