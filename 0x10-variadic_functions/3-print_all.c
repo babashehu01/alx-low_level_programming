263c97a2e00ad3d4ea092b0e227ebba68c57cefe
babashehu01 <shettimababashehu57@gmail.com>
2022-04-15 16:40:03 +0000

    Added a function that prints everything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * print_all - prints everything
+ * @format: format specifier
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int chars = strlen(format);
+	char separator[] = ", ";
+	int i;
+	va_list args;
+
+	va_start(args, format);
+
+	while (i < chars)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				printf("%s", va_arg(args, char *));
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i < chars - 1)
+			printf("%s", separator);
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
