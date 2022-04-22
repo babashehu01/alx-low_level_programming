21475071cb9ca69960ee447bfee89887c16eda0c
babashehu01 <shettimababashehu57@gmail.com>
2022-04-22 02:30:54 +0000

    Added a function that prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the structure
+ * Return: Length of node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
