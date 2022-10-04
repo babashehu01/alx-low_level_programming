aa81df91577627db6f334b8b85850c102b159804
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2022-10-04 07:52:16 +0100

    Fixing requirement fail error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		printf("Value checked array[%i] = [%d]\n", i, array[i]);
 
 		if (value == array[i])
 			return (i);
